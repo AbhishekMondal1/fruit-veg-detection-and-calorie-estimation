5b8355e124577385e6cc68709d1f67e6523359a3
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2021-12-14 23:59:46 +0530

    Add and modify Canvas image component

--- client/src/utils/CanvasDraw.js
+++ client/src/utils/CanvasDraw.js
@@ -1,37 +1,58 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import './canvasDraw.css';
 
-const CanvasDraw = (predictionData, imageUrl) => {
+const CanvasDraw = ({ predictionData, imageUrl }) => {
+  console.log("testdata", predictionData, imageUrl);
   const canvasref = useRef(null);
-    const ctx = canvasref.current.getContext("2d");
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "red";
-    ctx.font = "30px Arial";
-    const imageSrc = new Image();
-    imageSrc.src = imageUrl;
-    const imgWidth = imageSrc.width;
-    const imgHeight = imageSrc.height;
-    console.log("WxH", imgWidth, imgHeight);
-
-    var drawHeight = (imgHeight / imgWidth) * 1024;
-    ctx.clearRect(0, 0, 1024, 768);
+  const canvasimg = useRef(null);
+  const [cimg, setCimg] = useState("")
+  useEffect(() => {
+    const drawCanvas = async () => {
+      
+      const ctx = canvasref.current.getContext("2d");
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "red";
+      ctx.font = "30px Arial";
+      const imageSrc = new Image();
+      imageSrc.src = imageUrl;
+      const imgWidth = imageSrc.width;
+      const imgHeight = imageSrc.height;
+      console.log("WxH", imgWidth, imgHeight);
+      
+      var drawHeight = (imgHeight / imgWidth) * 1024;
+      ctx.clearRect(0, 0, 1024, 768);
     ctx.drawImage(imageSrc, 1, 1, 1024, drawHeight);
     ctx.beginPath();
-    predictionData?.forEach((pt) => {
+    await predictionData?.forEach((pt) => {
       ctx.rect(
         1024 * pt?.boundingBox.left,
         drawHeight * pt.boundingBox.top,
         1024 * pt.boundingBox.width,
         drawHeight * pt.boundingBox.height
-      );
-      console.log("ptp", pt);
-      return null;
-    });
-    ctx.stroke();
+        );
+        ctx.fillText(
+          pt.tagName,
+          1024 * pt.boundingBox.left,
+          drawHeight * pt.boundingBox.top
+          );
+          console.log("ptp", pt);
+          return null;
+        });
+        ctx.stroke();
+        //var canvasimg = new Image();
+        setCimg(canvasref.current.toDataURL({format: 'png', quality: 1}));
+        console.log(canvasref.current.toDataURL("image/png"))
+    }
+    drawCanvas();
+
+      }, [predictionData]);
+
   return (
     <div>
-          <canvas ref={canvasref} width={1024} height={768} />
+          <canvas className="canvas-draw" ref={canvasref} width={1024} height={768} />
+          <img className="canvas-img" ref={canvasimg} src={cimg} alt="" srcset=""/>
     </div>
   );
 };
 
-export default CanvasDraw
+export default CanvasDraw;
