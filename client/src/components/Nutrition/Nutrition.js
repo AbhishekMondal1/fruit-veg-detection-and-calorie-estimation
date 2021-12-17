0ca935c7bcf6cd55ada154cad51644325fe66bf0
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2021-12-17 23:58:34 +0530

    Add card slider at nutrition facts

--- client/src/components/Nutrition/Nutrition.js
+++ client/src/components/Nutrition/Nutrition.js
@@ -1,13 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import "./nutrition.css";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Nutrition = ({ predictionData }) => {
   const [nutritionValue, setNutritionValue] = useState([]);
   const [fruitNames, setFruitNames] = useState([]);
   const [fruitQuery, setFruitQuery] = useState({});
+  const [slideTrue, setSlideTrue] = useState(false);
   const CALORIENINJA_API = process.env.REACT_APP_CALORIENINJA_API;
   const CALORIENINJA_API_KEY = process.env.REACT_APP_CALORIENINJA_API_KEY;
 
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   console.table(predictionData);
 
   // get fruit, veg names
@@ -63,25 +86,71 @@ const Nutrition = ({ predictionData }) => {
 
   return (
     <div>
-      {nutritionValue &&
-        nutritionValue.map((n) => (
-          <div>
-            <p>calories: {n.calories}</p>
-            <p>name: {n.name}</p>
-            <p>protein: {n.protein_g}</p>
-            <p>carbohydrates_total_g: {n.carbohydrates_total_g}</p>
-            <p>sugar_g: {n.sugar_g}</p>
-            <p>fiber_g: {n.fiber_g}</p>
-            <p>serving_size_g:{n.serving_size_g}</p>
-            <p>sodium_mg: {n.sodium_mg}</p>
-            <p>potassium_mg: {n.potassium_mg}</p>
-            <p>fat_saturated_g: {n.fat_saturated_g}</p>
-            <p>fat_total_g: {n.fat_total_g}</p>
-            <p>cholesterol_mg: {n.cholesterol_mg}</p>
-            <br />
-            <hr />
-          </div>
-        ))}
+              <Carousel
+             swipeable={true}
+             draggable={false}
+             responsive={responsive}
+             minimumTouchDrag={10}
+             removeArrowOnDeviceType='mobile'>
+             
+        {nutritionValue && nutritionValue.map((n) => (
+            <div key={n.name} className="nutrition-container">
+              <div className="nutrition-row">
+                {/* <div className="nutrition-item">Name:</div>{" "} */}
+                <div className="nutrition-value"> {n.name} </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Calories:</div>{" "}
+                <div className="nutrition-value"> {n.calories} kcal </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Protein:</div>{" "}
+                <div className="nutrition-value"> {n.protein_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Carbohydrates total:</div>{" "}
+                <div className="nutrition-value">
+                  {" "}
+                  {n.carbohydrates_total_g} grams{" "}
+                </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Sugar:</div>{" "}
+                <div className="nutrition-value"> {n.sugar_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Fiber:</div>{" "}
+                <div className="nutrition-value"> {n.fiber_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Serving_size:</div>{" "}
+                <div className="nutrition-value"> {n.serving_size_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Sodium:</div>{" "}
+                <div className="nutrition-value"> {n.sodium_mg} mg </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Potassium:</div>{" "}
+                <div className="nutrition-value"> {n.potassium_mg} mg </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Fat (saturated):</div>{" "}
+                <div className="nutrition-value"> {n.fat_saturated_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Fat (total):</div>{" "}
+                <div className="nutrition-value"> {n.fat_total_g} grams </div>
+              </div>
+              <div className="nutrition-row">
+                <div className="nutrition-item">Cholesterol:</div>{" "}
+                <div className="nutrition-value"> {n.cholesterol_mg} mg </div>
+              </div>
+              <br />
+              <hr />
+            </div>
+          ))}
+        </Carousel>        
     </div>
   );
 };
