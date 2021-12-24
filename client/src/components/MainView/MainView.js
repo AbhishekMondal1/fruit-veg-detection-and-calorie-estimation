ad2421ddf6e7ceb61f3d8ecc7869c04685e82519
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2021-12-24 20:42:03 +0530

    Modify canvas size

--- client/src/components/MainView/MainView.js
+++ client/src/components/MainView/MainView.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "./mainView.css";
-import DrawBoundingBox from "../../utils/DrawBoundingBox";
+// import DrawBoundingBox from "../../utils/DrawBoundingBox";
+import Nutrition from "../Nutrition/Nutrition";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import CanvasDraw from "../../utils/CanvasDraw";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faBook, faImage, faChevronCircleLeft, faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Upload = () => {
-  const CUSTOM_VISION_API = process.env.REACT_APP_CUSTOM_VISION_API
-  const CUSTOM_VISION_PREDICTION_KEY = process.env.REACT_APP_CUSTOM_VISION_PREDICTION_KEY 
+  const CUSTOM_VISION_API = process.env.REACT_APP_CUSTOM_VISION_API;
+  const CUSTOM_VISION_PREDICTION_KEY = process.env.REACT_APP_CUSTOM_VISION_PREDICTION_KEY;
   const [image, setImage] = useState("");
   const [predictionData, setPredictionData] = useState([]);
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(false);
+  const [progressPercent, setProgressPercent] = useState(100);
 
-  const canvasRef = useRef(null);
   const imgRef = useRef(null);
-
+ 
   // update image section
   useEffect(() => {
     async function drawImage() {
-    if (image) {
+      if (image) {
         const url = await URL.createObjectURL(image);
         console.log("url", url);
         setImageUrl(url);
@@ -28,14 +34,14 @@ const Upload = () => {
       // URL.revokeObjectURL(image);
     };
   }, [image]);
-  
+
   // draw image in canvas
   useEffect(() => {
-    DrawBoundingBox(predictionData, imageUrl, canvasRef);
+    // DrawBoundingBox(predictionData, imageUrl, canvasRef);
     console.log("draw");
   }, [predictionData]);
 
-// Image File uploading for prediction
+  // Image File uploading for prediction
   const uploadFile = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -45,32 +51,40 @@ const Upload = () => {
     // setImageUrl(url);
     try {
       console.log("i", image);
-      const res = await axios.post(
-        CUSTOM_VISION_API,
-        image, {
+
+      const res = await axios.post(CUSTOM_VISION_API, image, {
         headers: {
           "content-type": "application/octet-stream",
           "Prediction-Key": CUSTOM_VISION_PREDICTION_KEY,
         },
+        onUploadProgress: (progressEvent) => {
+          var percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setProgressPercent(percentCompleted);
+          console.log("per", percentCompleted);
+        },
       });
-      console.log('res data',res.data.predictions);
+      console.log("res data", res.data.predictions);
       let predcdata = await res.data.predictions.filter((data) => {
-        if (data.probability > 0.60) {
+        if (data.probability > 0.6) {
           console.log(data.probability);
           return data;
         }
-      })
-      console.log('req prddata',predcdata);
+      });
+      console.log("req prddata");
+      console.table(predcdata);
       setPredictionData(predcdata);
       setLoading(false);
-
+      setImage("")
+      setImageUrl("")
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <div>
-      <form onSubmit={uploadFile}>
+    <div><br />
+      <form onSubmit={uploadFile} className="upload-image-form">
         <input
           className="select-file"
           type="file"
@@ -90,25 +104,22 @@ const Upload = () => {
         alt=""
         srcset=""
       />
+      {progressPercent < 100 ? (
+        <ProgressBar progressPercent={progressPercent} />
+      ) : (
+        ""
+      )}
       {loading && <h1>Loading...</h1>}
       <br />
-      { 
-        <canvas
-          ref={canvasRef}
-          width={1024}
-          height={768}
-          style={{
-            // position: "absolute",
-            // marginLeft: "auto",
-            // marginRight: "auto",
-            left: 10,
-            right: 0,
-            textAlign: "center",
-            zindex: 8,
-            // width: 800,
-            // height: 533,
-          }}
-        />
+      {
+        <div className="primary-feed">
+          {predictionData ? (
+            <CanvasDraw predictionData={predictionData} imageUrl={imageUrl} />
+            ) : (
+              ""
+              )}
+              {predictionData ? <Nutrition predictionData={predictionData} /> : ""}
+        </div>
       }
     </div>
   );
--- client/src/utils/CanvasDraw.js
+++ client/src/utils/CanvasDraw.js
@@ -1,56 +1,58 @@
 import React, { useEffect, useState, useRef } from "react";
-import './canvasDraw.css';
+import "./canvasDraw.css";
 
 const CanvasDraw = ({ predictionData, imageUrl }) => {
-  console.log("testdata", predictionData, imageUrl);
+  console.log("testdata", imageUrl);
+  console.table(predictionData);
   const canvasref = useRef(null);
   const canvasimg = useRef(null);
-  const [cimg, setCimg] = useState("")
+  const [cimg, setCimg] = useState("");
   useEffect(() => {
     const drawCanvas = async () => {
-      
       const ctx = canvasref.current.getContext("2d");
       ctx.lineWidth = 1;
-      ctx.strokeStyle = "red";
-      ctx.font = "30px Arial";
+      ctx.strokeStyle = "white";
+      ctx.font = "20px Poppins";
       const imageSrc = new Image();
       imageSrc.src = imageUrl;
       const imgWidth = imageSrc.width;
       const imgHeight = imageSrc.height;
       console.log("WxH", imgWidth, imgHeight);
-      
-      var drawHeight = (imgHeight / imgWidth) * 1024;
-      ctx.clearRect(0, 0, 1024, 768);
-    ctx.drawImage(imageSrc, 1, 1, 1024, drawHeight);
-    ctx.beginPath();
-    await predictionData?.forEach((pt) => {
-      ctx.rect(
-        1024 * pt?.boundingBox.left,
-        drawHeight * pt.boundingBox.top,
-        1024 * pt.boundingBox.width,
-        drawHeight * pt.boundingBox.height
+
+      var drawHeight = (imgHeight / imgWidth) * 700;
+      ctx.clearRect(0, 0, 700, 525);
+      ctx.drawImage(imageSrc, 1, 1, 700, drawHeight);
+      ctx.beginPath();
+      await predictionData?.forEach((pt) => {
+        ctx.rect(
+          700 * pt?.boundingBox.left,
+          drawHeight * pt.boundingBox.top,
+          700 * pt.boundingBox.width,
+          drawHeight * pt.boundingBox.height
         );
         ctx.fillText(
           pt.tagName,
-          1024 * pt.boundingBox.left,
+          700 * pt.boundingBox.left,
           drawHeight * pt.boundingBox.top
-          );
-          console.log("ptp", pt);
-          return null;
-        });
-        ctx.stroke();
-        //var canvasimg = new Image();
-        setCimg(canvasref.current.toDataURL({format: 'png', quality: 1}));
-        console.log(canvasref.current.toDataURL("image/png"))
-    }
+        );
+        console.log("ptp", pt);
+        return null;
+      });
+      ctx.stroke();
+      setCimg(canvasref.current.toDataURL({ format: "png", quality: 1 }));
+    };
     drawCanvas();
-
-      }, [predictionData]);
+  }, [predictionData]);
 
   return (
     <div>
-          <canvas className="canvas-draw" ref={canvasref} width={1024} height={768} />
-          <img className="canvas-img" ref={canvasimg} src={cimg} alt="" srcset=""/>
+      <canvas
+        className="canvas-draw"
+        ref={canvasref}
+        width={700}
+        height={525}
+      />
+      <img className="canvas-img" ref={canvasimg} src={cimg} alt="" srcset="" />
     </div>
   );
 };
