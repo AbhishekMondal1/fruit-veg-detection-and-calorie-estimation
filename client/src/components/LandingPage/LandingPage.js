fbdcf2a7501e89fae16ebf793f6cc64fd776197f
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2021-12-24 20:49:56 +0530

    Refactor Landing page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef } from "react";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom"
 import { gsap } from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faBook, faImage, } from "@fortawesome/free-solid-svg-icons";
 import "./landingPage.css";
 
 const LandingPage = () => {
+
   useEffect(() => {
     gsap.to(".overlay h1", {
       opacity: 0,
@@ -113,56 +115,64 @@ const LandingPage = () => {
       ease: "Expo.easeInOut",
       stagger: 0.08,
     });
-  });
+  },[]);
   return (
-      <div>
-          <div className="hero-image">
-              
-          </div>
-      <div class="overlay">
+    <div>
+      <div className="overlay">
         <h1>Fruit Vegetable Detector</h1>
         <span>ML AI</span>
       </div>
 
-      <div class="wrapper">
-        <div class="nav">
-          <div class="logo">
+      <div className="wrapper">
+        <div className="nav">
+          <div className="logo">
             <h1>
               <span>
                 calorie <br /> estimator
               </span>
               <br />
-              fruit detector 
+              fruit detector
             </h1>
           </div>
 
-          <div class="menu-links">
-            <ul>
-              <li>home.</li>
-              <li>fruit detect.</li>
-              <li>calorie burn counter.</li>
-            </ul>
-          </div>
+            <div className="menu-links">
+              <ul>
+                <li>
+                  <Link to="/"> home.</Link>
+                </li>
+                <li>
+                  <Link to="/detect">fruit detect.</Link>
+                </li>
+                <li>
+                  <Link to="/calorie-burn">calorie burn counter.</Link>
+                </li>
+              </ul>             
+            </div>
 
-          <div class="scrolldown">scroll</div>
+          <div className="scrolldown">scroll</div>
         </div>
 
-        <div class="text">
-          <div class="title">Detect</div>
+        <div className="text">
+          <div className="title">Detect</div>
           <p>
-            Detect the frits, vegetables and estimate their calories <br /> using Custom Vision AI.
+            Detect the frits, vegetables and estimate their calories <br />{" "}
+            using Custom Vision AI.
           </p>
         </div>
 
-        <div class="watchnow">
+        <div className="watchnow">
           <FontAwesomeIcon icon={faPlay} />
-          <a href="#">start now!</a>
+          <ul>
+                <li>
+                  <Link to="/detect"> Start now!</Link>
+                </li>
+          </ul>
         </div>
 
-        <div class="media">
+        <div className="media">
           <ul>
             <li>
-              <FontAwesomeIcon icon={faBook}  />
+              <FontAwesomeIcon icon={faBook} />
             </li>
             <li>
               <FontAwesomeIcon icon={faBook} />
@@ -173,14 +183,14 @@ const LandingPage = () => {
           </ul>
         </div>
 
-        <div class="ellipse-container">
-          <div class="ellipse thin"></div>
-          <div class="ellipse thick"></div>
-          <div class="ellipse yellow"></div>
-          <div class="circle1">
+        <div className="ellipse-container">
+          <div className="ellipse thin"></div>
+          <div className="ellipse thick"></div>
+          <div className="ellipse yellow"></div>
+          <div className="circle1">
             <span>Detect Fruits</span>
           </div>
-          <div class="circle2">
+          <div className="circle2">
             <span>Count Calorie Burn</span>
           </div>
         </div>
